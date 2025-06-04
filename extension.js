634824b2cb6de5f414d325dd10e37836d95928f5
Abbas Bayat <abasbayatalfa@gmail.com>
2025-06-04 10:33:10 +0330

    basic

---
+++ extension.js
@@ -0,0 +1,11 @@
+
+// make save button
+
+document.addEventListener("DOMContentLoaded", function() {
+    let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function()
+{
+    console.log("button clicked")
+})
+})
